aa10ad63f4830a814cee0e153775d8ff822c994b
Guilherme Muniz <gui.munizalemao@gmail.com>
2025-04-07 10:37:05 -0300

    feat: adiciona função de abrir e fechar menu

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 const searchResult = document.getElementById("search-result");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const mobileMenu = document.getElementById("mobile-menu");
+const closeMenu = document.getElementById("close-menu");
 
 // Função para exibir a mensagem de busca
 searchButton.addEventListener("click", function () {
@@ -11,4 +14,13 @@ searchButton.addEventListener("click", function () {
   } else {
     searchResult.textContent = ""; // Limpa a mensagem se o campo estiver vazio
   }
+});
+
+hamburgerMenu.addEventListener("click", () => {
+  console.log("aqui");
+  mobileMenu.style.transform = "translateX(0)"; // Abre o menu
+});
+
+closeMenu.addEventListener("click", () => {
+  mobileMenu.style.transform = "translateX(-100%)"; // Fecha o menu
 });
